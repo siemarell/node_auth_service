0acfefa7b5edba04a3bd28d6c2874796612b0b58
siem <siemarell@gmail.com>
2020-11-01 18:02:12 +0300

    tool config files

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@ import path from "path";
 import { config } from "dotenv";
 import { loadVar } from "./utils";
 
-config({ path: path.join(__dirname, "../.env") });
+config({path: path.join(__dirname, "../.env")});
 
 export const mongoUrl = loadVar("MONGO_URL");
 export const port = loadVar("PORT", true);
+
+export const enabledProviders = loadVar("ENABLED_PROVIDERS").split(',')
